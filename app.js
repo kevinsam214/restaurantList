51c34580224da459bb230ce59bb0226c6dea0596
kevinsam214 <kevinsam214@gmail.com>
2024-03-07 12:18:00 +0800

    feat: add search funtion to listing

--- app.js
+++ app.js
@@ -13,9 +13,20 @@ app.get('/', (req, res) => {
   res.redirect('/restaurant')
 })
 
-app.get('/restaurant' , (req,res) =>{
-  res.render('index' , {restaurants : restaurants})
-} )
+
+
+app.get('/restaurant',(req,res)=>{
+  const keyword = req.query.search?.trim() ;
+  const matchedRestaurant = keyword ? 
+  restaurants.filter(restaurant => {
+  return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
+  }
+  )
+  : restaurants ; //意思是如果keyword不存在的話 keyword就不會傳入matchedRestaurant  而matchedRestaurant會等於restaurants  而下面res.render()內的matchedRestaurant變成restaurants 這樣才不會有bug
+  res.render('index',{restaurants:matchedRestaurant,keyword})
+  
+})
+
 
 app.get('/restaurants/:id' , (req,res) =>{
   const id = req.params.id
@@ -26,3 +37,4 @@ app.get('/restaurants/:id' , (req,res) =>{
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })
+
